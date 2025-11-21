b571dc69a65841406df7b94928ccff9c111785de
DimasPramantya <igkdimas@gmail.com>
2025-11-21 11:22:31 +0700

    feat: save conversation

---
+++ src/main/java/com/example/websocketdemo/usecase/ConversationService.java
@@ -0,0 +1,58 @@
+package com.example.websocketdemo.usecase;
+
+import com.example.websocketdemo.domain.ChatMessage;
+import com.example.websocketdemo.domain.Conversation;
+import com.example.websocketdemo.domain.User;
+import com.example.websocketdemo.presentation.socketio.dto.MessageDto;
+import com.example.websocketdemo.repository.ChatMessageRepository;
+import com.example.websocketdemo.repository.ConversationRepository;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Service
+public class ConversationService {
+    private final ConversationRepository conversationRepository;
+    private final UserService userService;
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ConversationService(ConversationRepository conversationRepository, UserService userService, ChatMessageRepository chatMessageRepository) {
+        this.conversationRepository = conversationRepository;
+        this.userService = userService;
+        this.chatMessageRepository = chatMessageRepository;
+    }
+
+    @Cacheable(value = "getConversation", key = "{userOneId, userTwoId}")
+    public Conversation getConversation(Integer userOneId, Integer userTwoId){
+        User userOne = userService.getUserById(userOneId);
+        User userTwo = userService.getUserById(userTwoId);
+        return conversationRepository.findByUserOneAndUserTwo(userOne, userTwo);
+    }
+
+    public void saveConversationMessage(
+        Conversation conversation,
+        MessageDto message
+    ) {
+        chatMessageRepository.save(
+            ChatMessage.builder()
+                .createdAt(Timestamp.from(Instant.now()))
+                .conversation(conversation)
+                .content(message.getMessage())
+                .senderId(Integer.parseInt(message.getSenderId()))
+            .build()
+        );
+    }
+
+    @Cacheable(value = "getConversation", key = "{userOneId, userTwoId}")
+    public Conversation saveConversation(Integer userOneId, Integer userTwoId){
+        User userOne = userService.getUserById(userOneId);
+        User userTwo = userService.getUserById(userTwoId);
+        Conversation conversation = Conversation.builder()
+                .userOne(userOne)
+                .userTwo(userTwo)
+                .build();
+        return conversationRepository.save(conversation);
+    }
+}
--- src/main/java/com/example/websocketdemo/usecase/SocketService.java
+++ src/main/java/com/example/websocketdemo/usecase/SocketService.java
@@ -1,6 +1,7 @@
 package com.example.websocketdemo.usecase;
 
 import com.corundumstudio.socketio.SocketIOClient;
+import com.example.websocketdemo.domain.Conversation;
 import com.example.websocketdemo.presentation.socketio.dto.MessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,27 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class SocketService {
 
+    private final ConversationService conversationService;
+
+    public SocketService(ConversationService conversationService) {
+        this.conversationService = conversationService;
+    }
+
     public void sendMessage(MessageDto message, String eventName, SocketIOClient senderClient) {
         for (
             SocketIOClient client : senderClient.getNamespace().getRoomOperations(message.getUserTargetId()).getClients()) {
             if (!client.getSessionId().equals(senderClient.getSessionId())) {
+                Conversation conversation = conversationService.getConversation(
+                    Integer.parseInt(message.getSenderId()),
+                    Integer.parseInt(message.getUserTargetId())
+                );
+                if(conversation == null){
+                    conversation = conversationService.saveConversation(
+                        Integer.parseInt(message.getSenderId()),
+                        Integer.parseInt(message.getUserTargetId())
+                    );
+                }
+                conversationService.saveConversationMessage(conversation, message);
                 client.sendEvent(
                     eventName,
                     message
